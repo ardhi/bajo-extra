e650a3c5e7180b018a9edf4d04a86bbbbd7e80b9
Ardhi Lukianto <ardhi@lukianto.com>
2023-06-05 21:26:38 +0700

    Updates

--- bajo/init.js
+++ bajo/init.js
@@ -2,6 +2,10 @@ const pino = require('pino')
 
 module.exports = async function () {
   const { getConfig } = this.bajo.helper
-  const cfg = getConfig()
-  this.bajo.log = pino(getConfig('bajoExtra').log[cfg.dev ? 'dev' : 'prod'])
+  const gcfg = getConfig()
+  const cfg = getConfig('bajoExtra')
+  const log = cfg.log[gcfg.dev ? 'dev' : 'prod'] || {}
+  log.level = gcfg.logLevel
+  this.bajo.log = pino(log)
+  this.bajo.event.emit('boot', ['bajoExtraSwitchPino', 'Switched to \'Pino\' logger by bajoExtra', 'debug'])
 }
