ebe3c1928ae18c1251bb1594478db0f575a32471
Ardhi Lukianto <ardhi@lukianto.com>
2023-06-10 09:37:54 +0700

    Logger refactor

--- bajo/init.js
+++ bajo/init.js
@@ -1,19 +1,21 @@
 const pino = require('pino')
 
 module.exports = async function () {
-  const { getConfig, logLevels, _, isSet } = this.bajo.helper
-  const gcfg = getConfig()
+  const { getConfig, logLevels, _, log } = this.bajo.helper
+  const config = getConfig()
   const opts = getConfig('bajoExtra').log || {}
-  opts.level = gcfg.log.level
-  const log = pino(opts)
+  opts.level = config.log.level
+  if (_.get(opts, 'transport.target') === 'pino-pretty')
+    _.set(opts, 'transport.options.translateTime', `UTC:yyyy-mm-dd'T'HH:MM:ss.l'Z'`)
+  const instance = pino(opts)
   const logger = {}
   _.forOwn(logLevels, (v, k) => {
     logger[k] = ({ data, msg, args }) => {
+      if (config.env === 'dev' && k.length < 5) msg = _.pad('', 5 - k.length, ' ') + msg
       const params = _.isEmpty(data) ? [msg, ...args] : [data, msg, ...args]
-      log[k](...params)
+      instance[k](...params)
     }
   })
   this.bajoExtra.logger = logger
-
-  this.bajo.event.emit('boot', ['bajoExtraSwitchPino', 'Switched to \'Pino\' logger by bajoExtra', 'debug'])
+  log.debug(`Switched to 'Pino' logger`)
 }
