d10b42d071e37550b65d3acf535c27c21709311b
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-23 08:55:33 +0700

    Stringify object

--- bajo/helper/hash.js
+++ bajo/helper/hash.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt'
 import crypto from 'crypto'
 
 async function hash (text, type = 'md5', { digest = 'hex', salt = 10 } = {}) {
+  if (typeof text !== 'string') text = JSON.stringify(text)
   if (type === 'bcrypt') return await bcrypt.hash(text, salt)
   return crypto.createHash(type).update(text).digest(digest)
 }
