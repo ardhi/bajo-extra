0bf031b4d941f692321fbaf75dace952eb495976
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-14 11:08:17 +0700

    Updates

--- bajo/helper/hash.js
+++ bajo/helper/hash.js
@@ -1,10 +1,16 @@
 import bcrypt from 'bcrypt'
 import crypto from 'crypto'
 
-async function hash (text, type = 'md5', { digest = 'hex', salt = 10 } = {}) {
+async function hash (text, type = 'md5', options = {}) {
+  options.digest = options.digest ?? 'hex'
+  options.salt = options.hash ?? 10
   if (typeof text !== 'string') text = JSON.stringify(text)
-  if (type === 'bcrypt') return await bcrypt.hash(text, salt)
-  return crypto.createHash(type).update(text).digest(digest)
+  if (type === 'bcrypt') return await bcrypt.hash(text, options.salt)
+  if (type === 'short') {
+    type = 'shake256'
+    options.outputLength = 6
+  }
+  return crypto.createHash(type, options).update(text).digest(options.digest)
 }
 
 export default hash
